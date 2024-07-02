65f9f8180ec2574acfefcdd177c4378adf4e4486
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-07-02 10:25:54 +0200

    wip: analyzer

--- src/Analyzer/BaseAnalyzer.php
+++ src/Analyzer/BaseAnalyzer.php
@@ -10,6 +10,7 @@ abstract class BaseAnalyzer
     /**
      * @param  class-string  $class
      * @param  array<string, AnalyzerResult>  $results
+     * @throw \Exception
      */
     public static function analyze(string $class, array &$results, array &$additionalData = [], int $depth = 0): AnalyzerResult
     {
@@ -24,19 +25,23 @@ abstract class BaseAnalyzer
 
         // try to get navigation group
         try {
-            if(method_exists($class, 'getNavigationGroup')) {
-                $group =  $class::getNavigationGroup();
-                if($group) $result->tags[]  = $group;
-                
+            if (method_exists($class, 'getNavigationGroup')) {
+                $group = $class::getNavigationGroup();
+                if ($group) {
+                    $result->tags[] = $group;
+                }
+
             }
         } catch (\Exception $exception) {
         }
 
         // try invoke label
         try {
-            if(method_exists($class, 'getNavigationLabel')) {
+            if (method_exists($class, 'getNavigationLabel')) {
                 $label = $class::getNavigationLabel();
-                if($label) $result->label = $label;
+                if ($label) {
+                    $result->label = $label;
+                }
             }
         } catch (\BadMethodCallException $exception) {
         }
@@ -66,7 +71,9 @@ abstract class BaseAnalyzer
         }
         $handler = static::$handlers[$type ?? $class] ?? null;
         if ($handler) {
+            $copiedAdditionalData = $additionalData;
             $handler::analyze($class, $results, $additionalData, $depth + 1);
+            $additionalData = $copiedAdditionalData; // restore additional data as stack unwinds
         } else {
             echo 'No handler for ' . ($type ?? $class) . "\n";
         }
--- src/Analyzer/FilamentResourceAnalyzer.php
+++ src/Analyzer/FilamentResourceAnalyzer.php
@@ -2,6 +2,31 @@
 
 namespace BeraniDigitalID\FilamentAccess\Analyzer;
 
-class FilamentResourceAnalyzer extends BaseAnalyzer {
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Resource;
 
+class FilamentResourceAnalyzer extends BaseAnalyzer
+{
+    /**
+     * @param  class-string<resource>  $class
+     */
+    public static function analyze(string $class, array &$results, array &$additionalData = [], int $depth = 0): AnalyzerResult
+    {
+        $result = parent::analyze($class, $results, $additionalData, $depth);
+        $additionalData['tags'] = $result->tags;
+        $result->tags[] = $result->label;
+
+        foreach ($class::getRelations() as $relation) {
+            if (! is_string($relation)) {
+                continue;
+            }
+            $results = self::startAnalyze($relation, $results, $additionalData, $depth, RelationManager::class);
+        }
+
+        foreach ($class::getWidgets() as $widget) {
+            $results = self::startAnalyze($widget, $results, $additionalData, $depth, \Filament\Widgets\Widget::class);
+        }
+
+        return $result;
+    }
 }

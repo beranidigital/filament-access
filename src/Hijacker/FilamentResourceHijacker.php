6887e280ee32c6fd9f715d18a6d99dc6732e8f76
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-07-03 05:14:18 +0200

    fix: template FilamentResource

--- src/Hijacker/FilamentResourceHijacker.php
+++ src/Hijacker/FilamentResourceHijacker.php
@@ -9,7 +9,7 @@ class FilamentResourceHijacker extends BaseHijacker
     public static string $templateCode = <<<'PHP'
 <?php
 class  FilamentResourceHijacker {
-    public static function can(string $action, ?Model $record = null): bool
+    public static function can(string $action, ?\Illuminate\Database\Eloquent\Model $record = null): bool
     {
         if (static::shouldSkipAuthorization()) {
             return true;
@@ -18,8 +18,8 @@ class  FilamentResourceHijacker {
         $model = self::class;
 
         try {
-            return authorize($action, $record ?? $model, static::shouldCheckPolicyExistence())->allowed();
-        } catch (AuthorizationException $exception) {
+            return self::authorize($action, $record ?? $model, static::shouldCheckPolicyExistence())->allowed();
+        } catch (\Illuminate\Auth\Access\AuthorizationException $exception) {
             return $exception->toResponse()->allowed();
         }
 
